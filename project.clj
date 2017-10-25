a19dfc19ac3d8b22e92bf55805bc437c979da267
Torsten Uhlmann <tuhlmann@agynamix.de>
2017-10-25 11:18:58 +0200

    Not using AppleScriptEngine enymore as it stopped working on High Sierra.
    We are calling osascript now as an external process.

--- project.clj
+++ project.clj
@@ -3,13 +3,20 @@
   :url "http://agynamix.de"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.h2database/h2 "1.4.178"]
-                 [org.clojure/java.jdbc "0.3.3"]
-                 [java-jdbc/dsl "0.1.0"]
-                 [org.slf4j/slf4j-nop "1.7.7"]                 
-                 [com.jolbox/bonecp "0.8.0.RELEASE"]]
-  :plugins      [[lein-tar "3.2.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [com.h2database/h2 "1.4.196"]
+                 [org.clojure/java.jdbc "0.7.3"]
+                 [java-jdbc/dsl "0.1.3"]
+                 [org.slf4j/slf4j-nop "1.7.25"]
+                 [com.jolbox/bonecp "0.8.0.RELEASE"]
+                 [com.taoensso/timbre "4.10.0"]              ; Logging
+                 ]
+  
+  :plugins      [
+                 [lein-tar "3.2.0"]
+                 [lein-ancient "0.6.10"]
+                 ]
+
   :tar {:uberjar true}
   :aot :all
   :main itunes-media-import.main)
--- src/itunes_media_import/main.clj
+++ src/itunes_media_import/main.clj
@@ -1,10 +1,13 @@
 (ns itunes-media-import.main
-  (:require [clojure.edn :as edn]
-           [clojure.java.io :as io]
-           [clojure.java.jdbc :as sql]
-           [itunes-media-import.db-stuff :as db]
-           [itunes-media-import.itunes :as itunes])
-  (:use    [itunes-media-import.common])
+  (:require
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]
+    [clojure.java.jdbc :as sql]
+    [taoensso.timbre :as log]
+    [itunes-media-import.common :refer :all]
+    [itunes-media-import.db-stuff :as db]
+    [itunes-media-import.itunes :as itunes])
+
   (:gen-class))
 
 (defn load-config [filename]
@@ -20,42 +23,38 @@
   (let [file-root (:dir dir-config)
         all (doall (media-files file-root))
         dir-id (:dir-id dir-config)]
-    (println "sync " file-root)
+    (log/info "sync " file-root)
     ;; Cleanup removed files from itunes and DB
     (doseq [db-entry (db/all-entries dir-id)]
-      (def full-path-from-db (relative-to-full file-root (:path db-entry)))
-      ;(println "Check if file exists: " full-path-from-db)
-      (if-not (find-first #(= full-path-from-db (.getPath %)) all)
-        (do
-          ; movie removed, remove from db
-          (println "REMOVE: " full-path-from-db "with ID " (:perm_id db-entry))
-          (itunes/remove-itunes-media full-path-from-db (:perm_id db-entry))
-          (db/del-entry-by-id (:id db-entry))
-        )
-      )
-    )
+      (let [full-path-from-db (relative-to-full file-root (:path db-entry))]
+        (log/info "Check if file exists: " full-path-from-db)
+        (if-not (find-first #(= full-path-from-db (.getPath %)) all)
+          (do
+            ; movie removed, remove from db
+            (log/info "REMOVE: " full-path-from-db "with ID " (:perm_id db-entry))
+            (itunes/remove-itunes-media full-path-from-db (:perm_id db-entry))
+            (db/del-entry-by-id (:id db-entry))
+        ))))
     ;; Add new files to itunes and DB
     (doseq [f all]
-      (def full-path (.getPath f))
-      (def relative-path (full-to-relative  file-root full-path))
-      (if-not (db/contains-entry dir-id relative-path)
-        (do
-          ; entry not found add to itunes
-          (def itunes-result (itunes/itunes-media-add f (:kind dir-config)))
-          (if (:result-value itunes-result)
-            (db/add-entry dir-id relative-path (:permanent-id itunes-result))
-          )
-        )
-      )
-    )
+      (let [full-path (.getPath f)
+            relative-path (full-to-relative  file-root full-path)]
+        (if-not (db/contains-entry dir-id relative-path)
+          (do
+            ; entry not found add to itunes
+            (def itunes-result (itunes/itunes-media-add f (:kind dir-config)))
+            (if (:result-value itunes-result)
+              (db/add-entry dir-id relative-path (:permanent-id itunes-result))
+          )))))
   )
 )
 
 (defn -main [& args]
-  (println "Synchronize media directories with iTunes")
-  (def config (load-config (str (app-home) "/config.edn")))
-  (if-not (db/initialized?) (db/init-db))
-  (doseq [dir-config (:directories config)]
-    (sync-media-dir dir-config))
+  (log/info "Synchronize media directories with iTunes")
+  (let [config (load-config (str (app-home) "/config.edn"))]
+    (when-not (db/initialized?)
+      (db/init-db))
+    (doseq [dir-config (:directories config)]
+      (sync-media-dir dir-config)))
 )
 
